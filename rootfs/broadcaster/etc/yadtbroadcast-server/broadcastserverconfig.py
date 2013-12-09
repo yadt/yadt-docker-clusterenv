2809ef924adea6b7063ee13d0682dc6b9708b8e2
Arne Hilmann <arne@hilmann.de>
2013-12-09 12:07:20 +0100

    add start-broadcaster and its dependencies

---
+++ rootfs/broadcaster/etc/yadtbroadcast-server/broadcastserverconfig.py
@@ -0,0 +1,6 @@
+WS_PORT = 8081
+HTTP_PORT = 8080
+CACHE_FILE = '/var/log/broadcaster/cache'
+STORE_CACHE_AFTER_SECONDS = 60
+DOCROOT_DIR = '/var/yadtbroadcast-server/docroot'
+LOG_FILE = '/var/log/broadcaster/activity.log'
