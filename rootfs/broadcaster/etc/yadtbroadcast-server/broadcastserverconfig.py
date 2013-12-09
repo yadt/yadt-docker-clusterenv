430977385cd90f08ad736ecddbebd5796f50a76d
Arne Hilmann <arne@hilmann.de>
2013-12-09 13:43:22 +0100

    log to /tmp/logs

--- rootfs/broadcaster/etc/yadtbroadcast-server/broadcastserverconfig.py
+++ rootfs/broadcaster/etc/yadtbroadcast-server/broadcastserverconfig.py
@@ -1,6 +1,6 @@
 WS_PORT = 8081
 HTTP_PORT = 8080
-CACHE_FILE = '/var/log/broadcaster/cache'
+CACHE_FILE = '/tmp/logs/broadcaster/cache'
 STORE_CACHE_AFTER_SECONDS = 60
 DOCROOT_DIR = '/var/yadtbroadcast-server/docroot'
-LOG_FILE = '/var/log/broadcaster/activity.log'
+LOG_FILE = '/tmp/logs/broadcaster/activity.log'
